ffe49499dd25c98e744111113d2a7822e8d72aa5
Carmofrasao <anderfrasao52@gmail.com>
2022-08-07 10:54:39 -0300

    comunicação entre dois pc's em pitão

---
+++ client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import socket 
+# ip = input('digite o ip de conexao: ') 
+ip = '192.168.0.108' 
+port = 7000 
+addr = ((ip,port)) 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client_socket.connect(addr) 
+mensagem = input("digite uma mensagem para enviar ao servidor") 
+client_socket.send(mensagem.encode()) 
+print('mensagem enviada') 
+client_socket.close()
