dfce65352165c652e275ca7d66135dbbbca38676
Carmofrasao <anderfrasao52@gmail.com>
2022-08-07 11:59:41 -0300

    rede em anel para 4 pc's esta funcionando

--- pc1.py
+++ pc1.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python 
 
 import socket 
-host = '' 
-port = 7000 
-addr = (host, port) 
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-serv_socket.bind(addr) 
-serv_socket.listen(10) 
-print('aguardando conexao') 
-con, cliente = serv_socket.accept() 
-print('conectado')
-print("aguardando mensagem") 
-recebe = con.recv(1024) 
-print("mensagem recebida: "+ recebe.decode())
-serv_socket.close()
 
-ip = '192.168.0.108' 
-port = 7000 
-addr = ((ip,port)) 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-client_socket.connect(addr) 
+# Criar um soquete UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
 mensagem = input("digite uma mensagem para enviar ao servidor") 
-client_socket.send(mensagem.encode()) 
+
+addr = (('192.168.0.108',7000))
+
+client_socket.sendto(mensagem.encode(), addr) 
+
 print('mensagem enviada') 
-client_socket.close()
+
+# Criar um soquete UDP
+# Observe o uso de SOCK_DGRAM para pacotes UDP
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+# Atribuir endereço IP e número de porta ao soquete
+serv_socket.bind(('', 7003)) 
+
+print("aguardando mensagem") 
+
+# Recebendo o pacote do cliente junto com o endereço de onde ele está vindo
+message, address = serv_socket.recvfrom(1024)
+
+print("mensagem recebida: "+ message.decode())
