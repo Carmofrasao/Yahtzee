47d2f6e90a6fb369f60b1e00740edcb01777bfb0
aacf20 <aacf20@inf.ufpr.br>
2022-08-12 16:19:53 -0300

    arrumando numero do jogador

--- ring_logic.py
+++ ring_logic.py
@@ -202,7 +202,7 @@ def run_player(jogador, recv_port, send_port):
                 if jogador['aposta'] == mensage['aposta']:
                     # verifica se o jogador atual é quem vai fazer a jogada
                     print()
-                    print('O jogador ' + str(jogador['numero']) + ' vai jogar')
+                    print('O jogador ' + str(jogador['numero']+1) + ' vai jogar')
                     print()
                     jogador['fichas'] -= jogador['aposta']
                     mensage['resultado'] = jogada(mensage['jogada'])
@@ -225,7 +225,7 @@ def run_player(jogador, recv_port, send_port):
                         print()
                         mensage['fichas'] = jogador['fichas']
                         if jogador['fichas'] <= 0:
-                            print('O jogo acabou, o jogador numero '+str(jogador['numero'])+' esta sem ficha!')
+                            print('O jogo acabou, o jogador numero '+str(jogador['numero']+1)+' esta sem ficha!')
                             mensage['exit'] = 1
                             send_sock.sendto(json.dumps(mensage,indent=2).encode('utf-8'), ((LOCAL_HOST,send_port)))
                             exit()
